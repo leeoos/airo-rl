ac21777eb37456994e35d8273d868e407272cd74
leeoos <leoanrdo.colosi@gmail.com>
2023-11-20 22:49:15 +0100

    first progress

--- assignments/2/sarsa_lambda/student.py
+++ assignments/2/sarsa_lambda/student.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 
 def epsilon_greedy_action(env, Q, state, epsilon):
     # TODO choose the action with epsilon-greedy strategy
-    action = ...
+    action = env.action_space.sample() if np.random.uniform(0, 1) < epsilon else np.argmax(Q[state, :])
     return action
 
 
@@ -43,7 +43,13 @@ def sarsa_lambda(env, alpha=0.2, gamma=0.99, lambda_= 0.9, initial_epsilon=1.0, 
             next_action = epsilon_greedy_action(env, Q, next_state, epsilon)
 
             # TODO update q table and eligibility
-            ...
+            delta = reward + (gamma*(1-done)*Q[next_state,next_action]) - Q[state,action]
+            E[state,action] += 1
+            
+            for s in range(env.observation_space.n):
+                for a in range(env.action_space.n):
+                    Q[s,a] = Q[s,a] + alpha*delta*E[s,a]
+                    E[s,a] = gamma*lambda_*E[s,a]
 
             if not received_first_reward and reward > 0:
                 received_first_reward = True
@@ -53,6 +59,7 @@ def sarsa_lambda(env, alpha=0.2, gamma=0.99, lambda_= 0.9, initial_epsilon=1.0, 
             action = next_action
         
         # print(f"Episode {ep} finished after {ep_len} steps.")
+        E = np.zeros((env.observation_space.n, env.action_space.n))
 
         # update current epsilon
         if received_first_reward:
