88908d2aadb0386ebeb05e2cd247edfbb7d9c61a
leeoos <leonardo.colosi@gmail.com>
2023-12-08 01:16:05 +0100

    act and rollout

--- assignments/3/car_racing/student.py
+++ assignments/3/car_racing/student.py
@@ -39,21 +39,25 @@ class Policy(nn.Module):
         self.memory = None # rnn state is empy at the beginning
        
         # models
-        self.vae = VAE(latent_size=self.latent_dim)
+        self.vae = VAE(latent_size=self.latent_dim).to(self.device)
         self.rnn = MDN_RNN(
             input_size = self.latent_dim + self.action_space_dim, 
             output_size = self.latent_dim + self.action_space_dim
-        )
+        ).to(self.device)
         self.c = nn.Linear(
             in_features= self.latent_dim + self.hidden_dim, 
             out_features= self.action_space_dim
-        )
+        ).to(self.device)
+
+        # utils
+        self.trainer = Trainer()
+        self.roller = Rollout()
 
 
     def forward(self, x):
 
         if self.starting_state:
-            h = torch.zeros(1, self.hidden_dim)
+            h = torch.zeros(1, self.hidden_dim).to(self.device)
             self.starting_state = False
 
         else: h = self.memory
@@ -64,9 +68,7 @@ class Policy(nn.Module):
         # print("z: ", z.shape)
 
         c_in = torch.cat((z, h),dim=1).to(self.device)
-        # print("c_in: ", c_in.shape)
-
-        
+        # print("c_in: ", c_in.shape)       
         return c_in, z, h
     
     def act(self, state):
@@ -78,40 +80,133 @@ class Policy(nn.Module):
         # print("rnn_in: ", rnn_in.shape)
 
         self.memory, _ = self.rnn.forward_lstm(rnn_in)
-        self.memory = self.memory.squeeze(0)
+        self.memory = self.memory.squeeze(0).to(self.device)
         # print("memory: ", self.memory.shape)
         
         torch.clip(a, min = -1, max = 1 )
         return a.cpu().float().squeeze().detach().numpy()
 
     def train(self):
+        """Train the entire network or just the controller module"""
+
         # random rollout to collect observations
-        rollout_obs, rollout_actions = Rollout(self.env).random_rollout(self.num_rollout)
+        rollout_obs, rollout_actions = self.roller.random_rollout(self.env, self.num_rollout)
+        rollout_obs.to(self.device)
+        rollout_actions.detach().to(self.device)
 
         # train the vae
-        self.vae = Trainer().train(
+        self.vae = self.trainer.train(
             model_ =self.vae, 
             data_=rollout_obs, 
             batch_size_=self.batch_size,
-            retrain=False
+            retrain=True
         )
 
         # encode the observation to train the rnn
         mu, logvar = self.vae.encode(rollout_obs)
-        rollout_latent = self.vae.latent(mu, logvar)
-        rollout_al = torch.cat((rollout_actions, rollout_latent), dim=1)
+        rollout_latent = self.vae.latent(mu, logvar).to(self.device)
+        rollout_al = torch.cat((rollout_actions, rollout_latent), dim=1).to(self.device)
 
         # print(self.rnn.forward_lstm(rollout_al)[0].shape)
 
         # train the rnn
-        self.rnn = Trainer().train(
+        self.rnn = self.trainer.train(
             model_ =self.rnn, 
             data_=rollout_al.detach(), 
             batch_size_=self.batch_size,
-            retrain=False
+            retrain=True
         )
 
-        print(Rollout(self.env).rollout(self, self.c))
+        print(self.roller.rollout(env=self.env, agent=self, controller=self.c))
+
+        # train the controller
+        # pop_size = 4
+        # target_return= 950
+        # params = self.C.parameters()
+        # flat_params = torch.cat([p.detach().view(-1) for p in params], dim=0).cpu().numpy()
+        # print('test ', Rollout(self.env).rollout(self, self.c, params=flat_params))
+        # es = cma.CMAEvolutionStrategy(flat_params, 0.1, {'popsize': pop_size})
+        '''
+        cur_best = None
+        epoch = 0
+        log_step = 3
+        n_samples = 4
+        display = True
+        while not es.stop():
+            
+            if cur_best is not None and - cur_best > target_return:
+                print("Already better than target, breaking...")
+                break
+
+
+            r_list = [0] * pop_size  # result list
+            
+
+            solutions = es.ask()
+
+
+            # push parameters to queue
+            for s_id, s in enumerate(solutions):
+                for _ in range(n_samples):
+                    self.p_queue.put((s_id, s))
+
+            for _ in range(self.p_queue.qsize()):
+                s_id, params = self.p_queue.get()
+                self.r_queue.put((s_id, self.objective_function(params)))
+                
+          
+
+            # retrieve results
+            if display:
+                pbar = tqdm(total=pop_size * n_samples)
+
+            for _ in range(pop_size * n_samples):
+                while self.r_queue.empty():
+                    sleep(.1)
+
+                r_s_id, r = self.r_queue.get()
+                r_list[r_s_id] += r / n_samples
+
+                if display:
+                    pbar.update(1)
+
+            if display:
+                pbar.close()
+
+            es.tell(solutions, r_list)
+            es.disp()
+
+
+            # evaluation and saving
+            if epoch % log_step == log_step - 1:
+                best_params, best, std_best = self.evaluate(solutions, r_list, self.p_queue, self.r_queue)
+                print("Current evaluation: {}".format(best))
+
+                if not cur_best or cur_best > best:
+                    cur_best = best
+                    print("Saving new best with value {}+-{}...".format(-cur_best, std_best))
+                    
+                    # load_parameters(best_params, controller)
+
+                    # Load parameters
+                    for p, p_0 in zip(self.C.parameters(), params):
+                        p.data.copy_(p_0)
+                    controller = self.C
+                    # torch.save(
+                    #     {'epoch': epoch,
+                    #     'reward': - cur_best,
+                    #     'state_dict': controller.state_dict()},
+                    #     join(ctrl_dir, 'best.tar'))
+                if - best > target_return:
+                    print("Terminating controller training with value {}...".format(best))
+                    break
+
+
+            epoch += 1
+
+        # es.result_pretty()
+        self.e_queue.put('EOP')
+        '''
 
     def save(self):
         torch.save(self.state_dict(), 'model.pt')
--- assignments/3/car_racing/utils/rollout.py
+++ assignments/3/car_racing/utils/rollout.py
@@ -4,17 +4,18 @@ import torchvision.transforms as transforms
 
 class Rollout():
 
-    def __init__(self, env) -> None:
-        self.env = env 
-        self.env.reset()
+    def __init__(self):
+        pass
 
-    def random_rollout(self, num_rollout=1):
+    def random_rollout(self, env,num_rollout=1):
+        """ Execute a random rollout and returns actions and observations """
+
         rollout_obs = []
         rollout_actions = []
 
         for _ in range(num_rollout):
-            action = self.env.action_space.sample()
-            observation, reward, terminated, truncated, info = self.env.step(action)
+            action = env.action_space.sample()
+            observation, reward, terminated, truncated, info = env.step(action)
             done = terminated or truncated
             if done: break
 
@@ -29,29 +30,40 @@ class Rollout():
         rollout_actions = torch.stack(rollout_actions, dim=0)
         return rollout_obs, rollout_actions
     
+    def unflatten_parameters(self, params, example, device):
+        """ Unflatten parameters. NOte: example is generator of parameters (as returned by module.parameters()), used to reshape params """
 
-    def rollout(self, agent, controller, params=None, limit=100000):
+        params = torch.Tensor(params).to(device)
+        idx = 0
+        unflattened = []
+        for e_p in example:
+            unflattened += [params[idx:idx + e_p.numel()].view(e_p.size())]
+            idx += e_p.numel()
+        return unflattened
+
+    def rollout(self, env, agent, controller, params=None, limit=100000):
         """ Execute a rollout and returns minus cumulative reward. """
 
-        # copy params into the controller
         if params is not None:
-            # params = self.unflatten_parameters(params, self.C.parameters(), self.device)
+            params = self.unflatten_parameters(params, self.C.parameters(), self.device)
 
-            # load parameters
+            # load parameters into controller
             for p, p_0 in zip(controller.parameters(), params):
                 p.data.copy_(p_0)
 
-        obs, _ = self.env.reset()
+        obs, _ = env.reset()
         cumulative = 0
         i = 0
 
         while True:
             obs= torch.tensor(obs/255, dtype=torch.float).unsqueeze(0).permute(0,1,3,2).permute(0,2,1,3)
             action = agent.act(obs)
-            obs, reward, terminated, truncated, _ = self.env.step(action)
+            obs, reward, terminated, truncated, _ = env.step(action)
             done = terminated or truncated
 
             cumulative += reward
-            if done or i > limit: return - cumulative
+            if done or i >= limit: return - cumulative
             i += 1
+
+    
     
