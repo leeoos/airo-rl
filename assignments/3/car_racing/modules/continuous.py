f3aa5f455df6b6b27084f53b576ddfd588805a8d
leeoos <leoanrdo.colosi@gmail.com>
2023-12-14 14:19:40 +0100

    reward 300

--- assignments/3/car_racing/modules/continuous.py
+++ assignments/3/car_racing/modules/continuous.py
@@ -35,14 +35,17 @@ class Continuous(nn.Module):
                 remove(dest+self.name.lower()+'.pt')
         torch.save(self.state_dict(), dest+self.name.lower()+'.pt')
 
-    def load(self, dir): 
-        if exists(dir+self.name.lower()+'.pt'):
-            print("Loading model "+self.name+" state parameters")
-            self.load_state_dict(torch.load(dir+self.name.lower()+'.pt', map_location=self.device))
-            return self
-        else:
-            print("Error no model "+self.name.lower()+" found!")
-            exit(1)
+    def save(self, dest, f_value=None):
+        if not exists(dest): mkdir(dest)
+        else: 
+            if exists(dest+self.name.lower()+'.pt'):
+                remove(dest+self.name.lower()+'.pt')
+        torch.save(
+            {
+                'state_dict': self.state_dict(),
+                'f_value': f_value
+            }, dest+self.name.lower()+'.pt'
+        )
 
     def load(self, dir, get_value=None): 
         if exists(dir+self.name.lower()+'.pt'):
